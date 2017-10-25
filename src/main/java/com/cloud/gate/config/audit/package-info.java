e53a3c9524cc4acd79d2eff5ad502880200a7972
Bob Ying <yingmingbo@315pr.com>
2017-10-25 14:05:10 +0800

    Initial application generated by JHipster-4.10.0

---
+++ src/main/java/com/cloud/gate/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.cloud.gate.config.audit;
---
+++ src/main/java/com/cloud/gate/security/SpringSecurityAuditorAware.java
@@ -0,0 +1,19 @@
+package com.cloud.gate.security;
+
+import com.cloud.gate.config.Constants;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Implementation of AuditorAware based on Spring Security.
+ */
+@Component
+public class SpringSecurityAuditorAware implements AuditorAware<String> {
+
+    @Override
+    public String getCurrentAuditor() {
+        String userName = SecurityUtils.getCurrentUserLogin();
+        return userName != null ? userName : Constants.SYSTEM_ACCOUNT;
+    }
+}

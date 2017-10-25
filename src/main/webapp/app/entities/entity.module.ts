c09028d41f19ba3880d3a314f43aa88b39da3aee
Bob Ying <bobying76@gmail.com>
2017-10-25 20:24:56 +0800

    add entity industry

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -2,12 +2,14 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { GateArea_typeModule } from './area-type/area-type.module';
 import { GateAreaModule } from './area/area.module';
+import { GateIndustryModule } from './industry/industry.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
         GateArea_typeModule,
         GateAreaModule,
+        GateIndustryModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
---
+++ src/test/javascript/spec/app/entities/industry/industry-detail.component.spec.ts
@@ -0,0 +1,61 @@
+/* tslint:disable max-line-length */
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { DatePipe } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Rx';
+import { JhiDateUtils, JhiDataUtils, JhiEventManager } from 'ng-jhipster';
+import { GateTestModule } from '../../../test.module';
+import { MockActivatedRoute } from '../../../helpers/mock-route.service';
+import { IndustryDetailComponent } from '../../../../../../main/webapp/app/entities/industry/industry-detail.component';
+import { IndustryService } from '../../../../../../main/webapp/app/entities/industry/industry.service';
+import { Industry } from '../../../../../../main/webapp/app/entities/industry/industry.model';
+
+describe('Component Tests', () => {
+
+    describe('Industry Management Detail Component', () => {
+        let comp: IndustryDetailComponent;
+        let fixture: ComponentFixture<IndustryDetailComponent>;
+        let service: IndustryService;
+
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                imports: [GateTestModule],
+                declarations: [IndustryDetailComponent],
+                providers: [
+                    JhiDateUtils,
+                    JhiDataUtils,
+                    DatePipe,
+                    {
+                        provide: ActivatedRoute,
+                        useValue: new MockActivatedRoute({id: 123})
+                    },
+                    IndustryService,
+                    JhiEventManager
+                ]
+            }).overrideTemplate(IndustryDetailComponent, '')
+            .compileComponents();
+        }));
+
+        beforeEach(() => {
+            fixture = TestBed.createComponent(IndustryDetailComponent);
+            comp = fixture.componentInstance;
+            service = fixture.debugElement.injector.get(IndustryService);
+        });
+
+        describe('OnInit', () => {
+            it('Should call load all on init', () => {
+            // GIVEN
+
+            spyOn(service, 'find').and.returnValue(Observable.of(new Industry(10)));
+
+            // WHEN
+            comp.ngOnInit();
+
+            // THEN
+            expect(service.find).toHaveBeenCalledWith(123);
+            expect(comp.industry).toEqual(jasmine.objectContaining({id: 10}));
+            });
+        });
+    });
+
+});

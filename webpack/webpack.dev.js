8ff61b31c306ff7366e5b60fe579608b9e554d1d
bob.ying <124599792@qq.com>
2017-11-20 12:28:22 +0800

    add dict's entity

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -19,6 +19,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
                 '/uaa',
                 '/param',
                 '/user',
+                '/translate',
                 /* jhipster-needle-add-entity-to-webpack - JHipster will add entity api paths here */
                 '/api',
                 '/management',
